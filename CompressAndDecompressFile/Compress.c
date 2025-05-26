76e7f87fb79d382bc62256ad30f309ae8121b09a
Hakob650 <hakob11meneshian@gmail.com>
2025-05-27 01:32:39 +0400

    Create Compress.c

---
+++ CompressAndDecompressFile/Compress.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "rle.h"
+void Compress(const char *input,const char *output){
+    FILE *in=fopen(input,"r");
+    if(!in){
+        printf("Failed to open the file");
+        return;
+    }
+    FILE *out=fopen(output,"w");
+    if(!out){
+        printf("Failed to open the file");
+        return;
+    }
+    int curr=fgetc(in);
+    while(curr!=EOF){
+        int count=1;
+        int nextchar;
+        while((nextchar=fgetc(in))==curr && count<256){
+                count++;
+        }
+        fputc(curr,out);
+        fputc(count,out);
+        curr=nextchar;
+    }
+    fclose(in);
+    fclose(out);
+
+    printf("Compressed successfully");
+}
