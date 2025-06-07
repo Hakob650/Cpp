52435e3666e2d07265210f0fa2be55887660cd1c
Hakob650 <hakob11meneshian@gmail.com>
2025-06-07 17:41:35 +0400

    Update BankSystem.cpp

--- BankAccountSystem/BankSystem.cpp
+++ BankAccountSystem/BankSystem.cpp
@@ -1,54 +1,54 @@
-#include "BankSystem.h"
 #include <iostream>
+#include "BankSystem.h"
 
 BankSystem::~BankSystem()
 {
-	for(size_t i = 0; i < accounts.size(); ++i)
-	{
-		delete accounts[i];
-	}
+    for(size_t i = 0; i < accounts.size(); ++i)
+    {
+        delete accounts[i];
+    }
 }
-void BankSystem::add_account(const std::string& name,int accNum,double initialBalance)
+
+void BankSystem::add_account(const std::string& name,int accNum,double bal)
 {
-	BankAccount* new_account = new BankAccount(name,accNum,initialBalance);
-	accounts.push_back(new_account);
+    BankAccount* new_account = new BankAccount(name,accNum,bal);
+    accounts.push_back(new_account);
 }
 
 BankAccount* BankSystem::find_account(int accNum)
 {
-	for(size_t i = 0; i < accounts.size(); ++i)
-	{
-		if(accounts[i]->getAccountNumber() == accNum)
-		{
-			return accounts[i];	
-		}
-	}
-	std::cout<<"Account "<<accNum<<"not found";
-	return nullptr;
+    for(size_t i = 0; i < accounts.size(); ++i)
+    {
+        if((accounts[i]->getAccountNumber()) == accNum)
+        {
+            return accounts[i];
+        }
+    }
+        std::cout<<"Account "<<accNum<<" not found";
+        return nullptr;
 }
 
 void BankSystem::transfer_funds(int senderAcc,int recieverAcc,double amount)
 {
-	BankAccount* sender = find_account(senderAcc);
-	BankAccount* reciever = find_account(recieverAcc);
-
-	if(sender && reciever)
-	{
-		if(sender->withdraw(amount))
-		{
-			reciever->deposit(amount);
-			std::cout<<"Transferred: "<<amount<<std::endl;
-		}
-	}
+    BankAccount* sender = find_account(senderAcc);
+    BankAccount* reciever = find_account(recieverAcc);
+    if(sender && reciever)
+    {
+        if(sender->withdraw(amount))
+        {
+            reciever->deposit(amount);
+            std::cout<<"Transferred "<<amount<<std::endl;
+        }
+    }
 }
 
 void BankSystem::displayAllAccounts()
 {
-	std::cout<<"All Bank Accounts"<<std::endl;
-	for(size_t i = 0; i < accounts.size(); ++i)
-	{
-		accounts[i]->display();
-	}
+    std::cout<<"All bank accounts: "<<std::endl;
+    for(size_t i = 0; i < accounts.size(); ++i)
+    {
+        accounts[i]->display();
+    }
 }
 
 
