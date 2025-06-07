ca352dd379bb72c3acf9174b82a9ce99fe40e415
Hakob650 <hakob11meneshian@gmail.com>
2025-06-07 17:37:46 +0400

    Update BankAccount.h

--- BankAccountSystem/BankAccount.h
+++ BankAccountSystem/BankAccount.h
@@ -1,30 +1,22 @@
-#ifndef BANK_ACCOUNT_H
-#define BANK_ACCOUNT_H
-
 #include <string>
-#include <initializer_list>
 
 class BankAccount
 {
 private:
-	std::string accountHolder;
-	int accountNumber;
-	double balance;
+    std::string accountHolder;
+    int accountNumber;
+    double balance;
 public:
-	BankAccount();
-	BankAccount(const std::string& name,int accNum,double initbal);
-	BankAccount(const BankAccount& other);
-	BankAccount(const std::string& name,std::initializer_list<double> values);
-	~BankAccount();
+    BankAccount();
+    BankAccount(std::string name,int accNum,double bal);
+    BankAccount(const BankAccount& other);
+    ~BankAccount();
 
-	void deposit(double amount);
-	bool withdraw(double amount);
-	void display() const;
+    void deposit(double amount);
+    bool withdraw(double amount);
+    void display() const;
 
-	int getAccountNumber() const;
+    int getAccountNumber() const;
 };
 
 #endif
-	
-
-	
