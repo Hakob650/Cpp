67be8509f7b221be716c973cb54b042c1971c1c2
Hakob650 <hakob11meneshian@gmail.com>
2025-06-10 03:20:06 +0400

    Update string.h

--- June2/StringClass/string.h
+++ June2/StringClass/string.h
@@ -1,7 +1,7 @@
-#ifndef STRING_H
-#define STRING_H
-
 #include <cstddef>
+
+#ifndef STRING_HPP
+#define STRING_HPP
 
 class String
 {	
@@ -9,14 +9,17 @@ private:
 	char* data_;
 	size_t size;
 public:
-	String(); 
-	String(const char* str); 
-	~String(); 
+	 String(); 
+	 String(const char* str); 
+    	 String(const String& other);
+    	 String(String&& other);
+    	 String& operator=(const String& other);
+   	 String& operator=(String&& other);
+   	 ~String(); 
 		
-	size_t length() const; 
-	const char* c_str() const; 
-	void print() const; 
+	 size_t length() const; 
+ 	 const char* c_str() const; 
+ 	 void print() const; 
 };
 
-#endif
-
+#endi
