462c67d236c623bd0d89d83f817de308b32607d8
Hakob650 <hakob11meneshian@gmail.com>
2025-06-12 21:42:34 +0400

    Create IsomorphicStrings.hpp

---
+++ LeetCode(C++)/IsomorphicStrings/IsomorphicStrings.hpp
@@ -0,0 +1,12 @@
+#ifndef ISOMORPHIC_STRINGS_HPP
+#define ISOMORPHIC_STRINGS_HPP
+
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+    bool isIsomorphic(std::string s, std::string t);
+};
+
+#endif 
