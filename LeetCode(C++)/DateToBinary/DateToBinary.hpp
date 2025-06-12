49a6d283f45c786b0959a1e51e6f51a21107fac9
Hakob650 <hakob11meneshian@gmail.com>
2025-06-12 21:36:13 +0400

    Create DateToBinary.hpp

---
+++ LeetCode(C++)/DateToBinary/DateToBinary.hpp
@@ -0,0 +1,14 @@
+#ifndef DATE_TO_BINARY_HPP
+#define DATE_TO_BINARY_HPP
+
+#include <string>
+
+class Solution {
+private:
+    std::string toBinary(int num);
+    
+public:
+    std::string dateToBinary(std::string date);
+};
+
+#endif 
