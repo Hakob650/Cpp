c74adf7af3b235995cd6646fb51c9f1b259e2a4b
Hakob650 <hakob11meneshian@gmail.com>
2025-06-12 20:29:20 +0400

    Create MinimumCommonValue.hpp

---
+++ LeetCode(C++)/MinimumCommonValue/MinimumCommonValue.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+#ifndef MINIMUM_COMMON_VALUE_HPP
+#define MINIMUM_COMMON_VALUE_HPP
+
+class Solution
+{
+    public:
+        int getCommon(std::vector<int> &nums1, std::vector<int> &nums2);
+};
+
+#endif
