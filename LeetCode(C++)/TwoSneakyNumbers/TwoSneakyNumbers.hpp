aaa42d066bb3098e359962ec33bea2c4562348d4
Hakob650 <hakob11meneshian@gmail.com>
2025-06-12 21:38:24 +0400

    Create TwoSneakyNumbers.hpp

---
+++ LeetCode(C++)/TwoSneakyNumbers/TwoSneakyNumbers.hpp
@@ -0,0 +1,11 @@
+#ifndef TWO_SNEAKY_NUMBERS_HPP
+#define TWO_SNEAKY_NUMBERS_HPP
+
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> getSneakyNumbers(std::vector<int>& nums);
+};
+
+#endif 
