c01d6c2bba21152e06a897a44a9eb929fa6c771d
Hakob650 <hakob11meneshian@gmail.com>
2025-06-12 21:50:03 +0400

    Create DifferenceOfTwoArrays.hpp

---
+++ LeetCode(C++)/DifferenceOfTwoArrays/DifferenceOfTwoArrays.hpp
@@ -0,0 +1,12 @@
+#ifndef DIFFERENCE_OF_TWO_ARRAYS_HPP
+#define DIFFERENCE_OF_TWO_ARRAYS_HPP
+
+#include <vector>
+
+class Solution {
+public:
+    bool arr_contains(std::vector<int>& arr, int val);
+    std::vector<std::vector<int>> findDifference(std::vector<int>& nums1, std::vector<int>& nums2);
+};
+
+#endif 
