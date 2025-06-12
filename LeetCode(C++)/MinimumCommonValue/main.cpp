2799610f419f9f4043bec612f7f81ecc7ccbfaf7
Hakob650 <hakob11meneshian@gmail.com>
2025-06-12 20:25:58 +0400

    Update main.cpp

--- LeetCode(C++)/MinimumCommonValue/main.cpp
+++ LeetCode(C++)/MinimumCommonValue/main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <vector>
 
+int main()
+{
+    Solution sol;
+    std::vector<int> nums1 = {1, 2, 3, 4, 5};
+    std::vector<int> nums2 = {5, 6, 7, 8, 9};
+
+    int commonValue = sol.getCommon(nums1, nums2);
+    if (commonValue != -1)
+    {
+        std::cout << "Common value: " << commonValue << std::endl;
+    }
+    else
+    {
+        std::cout << "No common value found." << std::endl;
+    }
+
+    return 0;
+}
