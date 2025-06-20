2516c8583fa8890006811f8f90f6036f31f01035
Hakob650 <hakob11meneshian@gmail.com>
2025-06-20 18:15:03 +0400

    Create customer.hpp

---
+++ ResturantOrderingSystem/include/customer.hpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include "order.hpp"
+
+class Customer
+{
+private:
+        std::string name;
+        std::string contactInfo;
+        std::vector <Order> orderHistory;
+public:
+        Customer(std::string customerName, std::string contact);
+        Customer(const Customer& other);
+        Customer& operator=(const Customer& other);
+        Customer(Customer&& other) noexcept;
+        Customer& operator=(Customer&& other) noexcept;
+        ~Customer() = default;
+        void placeOrder(Order order);
+        void viewOrderHistory() const;
+        std::string getName() const;
+};
