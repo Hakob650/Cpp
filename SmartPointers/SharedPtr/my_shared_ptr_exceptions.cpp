34dccccdbf746c6d401ed94c02f96bbc8b1d964a
Hakob650 <hakob11meneshian@gmail.com>
2025-07-13 20:51:59 +0400

    Delete SmartPointers/SharedPtr/my_shared_ptr_exceptions.cpp

--- SmartPointers/SharedPtr/my_shared_ptr_exceptions.cpp
+++
@@ -1,9 +0,0 @@
-#include "my_shared_ptr_exceptions.hpp"
-
-SharedPtrException::SharedPtrException(const std::string& msg)
-    : message(msg) {}
-
-const char* SharedPtrException::what() const noexcept
-{
-    return message.c_str();
-}
